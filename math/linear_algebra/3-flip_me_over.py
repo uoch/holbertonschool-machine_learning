77908ce63f231a2d11f9c7ab25e664ee2aab8d9f
uoch <chaouch.thameurtc61@gmail.com>
2023-07-24 16:55:25 +0100

    t

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 """returns the transpose of a 2D matrix"""
-
-
 def matrix_transpose(matrix):
     """returns the transpose of a 2D matrix"""
-    xx = matrix.copy()
-    rows = len(xx)
-    cols = len(xx[0])
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    # Create a new matrix to store the transposed elements
+    xx = [[0 for _ in range(rows)] for _ in range(cols)]
 
     for i in range(rows):
-        for j in range(i + 1, cols):  # Only loop over elements above the main diagonal
-            x = xx[i][j]
-            xx[i][j] = xx[j][i]
-            xx[j][i] = x
+        for j in range(cols):
+            xx[j][i] = matrix[i][j]
+
     return xx
