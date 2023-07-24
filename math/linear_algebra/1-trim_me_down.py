7c23de152a1ff092fb81755f1d824e9226be532c
uoch <chaouch.thameurtc61@gmail.com>
2023-07-24 11:36:19 +0100

    trim

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = matrix[1]
+a=5
+c = 6
 x = [the_middle[i:i+2] for i in range(0, len(the_middle), 2)]
