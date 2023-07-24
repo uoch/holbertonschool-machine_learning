7aa7262cf8dc053cda0235f33da260e06842261f
uoch <chaouch.thameurtc61@gmail.com>
2023-07-24 16:07:25 +0100

    size

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""recursive method:
+def matrix_shape(matrix):
+    """recursive method:
 def matrix_shape(matrix):
     if isinstance(matrix, list):
         return [len(matrix)] + matrix_shape(matrix[0])
     else:
         return []
     """
-def matrix_shape(matrix):
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
