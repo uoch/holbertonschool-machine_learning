7b250cb17cdd4d00e38b5b656971f4936d0cc84b
uoch <chaouch.thameurtc61@gmail.com>
2023-07-25 09:14:56 +0100

    specified

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -7,9 +7,18 @@ def cat_matrices2D(mat1, mat2, axis=0):
     x = [row.copy() for row in mat1]
     y = [row.copy() for row in mat2]
 
+    if mat1 is None or mat2 is None:
+        return None
+
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return x + y
     elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         for i in range(len(x)):
             x[i].extend(y[i])
         return x
+    else:
+        return None
