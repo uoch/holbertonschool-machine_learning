64fce71535af692afcad16764f335507905f20e2
Your Name <you@example.com>
2023-07-25 14:36:51 +0100

    wise

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""that performs element-wise addition, subtraction, multiplication, and division"""
+
+
+def np_elementwise(mat1, mat2):
+    """add,sub,mul,div"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
