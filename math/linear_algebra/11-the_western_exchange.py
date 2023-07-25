fca39c1b642eb6b48fbeb5119ddb7328c06cfce4
Your Name <you@example.com>
2023-07-25 12:34:25 +0100

    transpose

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """returns the transpose of the matrix"""
-import numpy as np
 
 
 def np_transpose(matrix):
     """returns the transpose of the matrix"""
+    import numpy as np
     return np.transpose(matrix)
