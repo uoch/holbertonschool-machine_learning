4882c43964d992c2240a23d2d0912f1265369480
Your Name <you@example.com>
2023-07-28 15:11:28 +0100

    add

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python3
-"""that adds two matrices"""
-import numpy as np
+"""that performs matrix multiplication"""
+
+
+def matrix_shape(matrix):
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
 
 
 def add_matrices(mat1, mat2):
-    """adds two matrices"""
-    x = np.array(mat1)
-    y = np.array(mat2)
-    if x.shape != y.shape:
+    """Adds two matrices represented as lists."""
+    if not (isinstance(mat1, list) and isinstance(mat2, list)):
+        return None
+
+    if not (isinstance(mat1[0], list) and isinstance(mat2[0], list)):
+        if len(mat1) != len(mat2):
+            return None
+        else:
+            return [mat1[i] + mat2[i] for i in range(len(mat1))]
+
+    # Check if both matrices have the same shape
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
+    if shape1 != shape2:
         return None
     else:
-        return (x+y).tolist()
+        # Initialize the result list
+        Badd = []
+        for i in range(len(shape1)):
+            # Check if both rows have the same number of columns
+            if len(mat1[i]) != len(mat2[i]):
+                return None
+            # Perform element-wise addition and store the result row
+            add_row = [mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))]
+            Badd.append(add_row)
+
+        return Badd
