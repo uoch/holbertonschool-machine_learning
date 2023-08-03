62464f6981bea40cc975316df7f333e5c800096b
uoch <chaouch.thameurtc61@gmail.com>
2023-08-03 14:25:56 +0100

    integrale

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,14 +4,18 @@
 
 def poly_integral(poly, C=0):
     """return sum(poly[i] * (i + 1) ** C for i in range(len(poly) - 1))"""
-    if not isinstance(poly, list) or len(poly) < 1:
+    if not isinstance(poly, list):
         return None
-    x = []
-    x.append(C)
-    for i in range(len(poly)):
-        k = poly[i] / (i+1)
-        if k.is_integer():
-            x.append(int(k))
-        else:
-            x.append(k)
-    return x
+    x= []
+    if len(poly) == 1 and isinstance(C, int):
+        return [C]
+    elif len(poly) > 1:
+        x = []
+        x.append(C)
+        for i in range(len(poly)):
+            k = poly[i] / (i+1)
+            if k.is_integer():
+                x.append(int(k))
+            else:
+                x.append(k)
+        return x
