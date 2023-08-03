62eb6868d74af9d61ca4bfb6b73661f74825980a
uoch <chaouch.thameurtc61@gmail.com>
2023-08-03 13:44:44 +0100

    polydiv

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -3,13 +3,13 @@
 
 
 def poly_derivative(poly):
+    """
+    return [poly[i+1] - poly[i] for i in range(len(poly)-1)]
+    """
     if not isinstance(poly, list) or len(poly) < 1:
         return None
     if len(poly) == 1:
         return [0]
-    """
-    return [poly[i+1] - poly[i] for i in range(len(poly)-1)]
-    """
     x = []
     for i in range(1, len(poly)):
         x.append(poly[i] * (i))
