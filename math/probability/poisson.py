0d25708ca61bb770f8ce0316afa3c6e9a27d678b
uoch <chaouch.thameurtc61@gmail.com>
2023-08-07 11:58:28 +0100

    lambda

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -6,7 +6,6 @@ class Poisson:
     """represents a poisson distribution"""
 
     def __init__(self, data=None, lambtha=1.):
-        """initialize a poisson distribution"""
         self.data = data
         if data is None:
             self.lambtha = float(lambtha)
@@ -19,7 +18,7 @@ class Poisson:
                 self.lambtha = float(lambtha)
                 if lambtha <= 0.0:
                     raise ValueError("lambtha must be a positive value")
-                if not isinstance(data, list) or len(data) <= 1:
+                if len(data) <= 1:
                     raise ValueError("data must contain multiple values")
                 s = sum(data)
                 self.lambtha = s / len(data)
