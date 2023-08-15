d226542902bf44e0c0aefe1a71543e82859623b6
uoch <chaouch.thameurtc61@gmail.com>
2023-08-15 17:30:29 +0100

    deep_deep

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -14,8 +14,6 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(layer, int) and layer > 0 for layer in layers):
-            raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)  # Number of layers
         self.cache = {}  # Store intermediate values during forward propagation
@@ -24,6 +22,9 @@ class DeepNeuralNetwork:
 
         # Initialize weights and biases using He initialization
         for l, layer_size in enumerate(layers, start=1):
-            self.weights[f"W{l}"] = np.random.randn(
-                layer_size, nx if l == 1 else layers[l - 2]) * np.sqrt(2 / (nx if l == 1 else layers[l - 2]))
-            self.biases[f"b{l}"] = np.zeros((layer_size, 1))
+            if not isinstance(layer_size, int):
+                raise TypeError("layers must be a list of positive integers")
+            else:
+                self.weights[f"W{l}"] = np.random.randn(
+                    layer_size, nx if l == 1 else layers[l - 2]) * np.sqrt(2 / (nx if l == 1 else layers[l - 2]))
+                self.biases[f"b{l}"] = np.zeros((layer_size, 1))
