a5873fb9c3556aaab28979d9505a10f0892174ad
uoch <chaouch.thameurtc61@gmail.com>
2023-08-17 14:42:24 +0100

    one_hot

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -5,5 +5,11 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """One-hot encoding of the given classes"""
-    result = np.eye(classes)[Y]
-    return result
+    if not isinstance(Y, np.ndarray) or len(Y.shape) != 1:
+        return None
+    if not isinstance(classes, int) or classes < 2:
+        return None
+    re = np.zeros((classes, Y.shape[0]))
+    for i, y in enumerate(Y):
+        re[y, i] = 1
+    return re
