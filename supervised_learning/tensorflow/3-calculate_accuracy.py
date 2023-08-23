f72b6d01776df5401819fd28d65d62c96915446b
uoch <chaouch.thameurtc61@gmail.com>
2023-08-23 17:08:35 +0100

    accuracy

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """tensorflow project"""
-import tensorflow as tf
+# import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 
 def calculate_accuracy(y, y_pred):
     """Calculates the accuracy"""
-    x = tf.shape(y)[0]
+    x= tf.shape(y)[0]
     y_pred = tf.argmax(y_pred, axis=1)
     y = tf.argmax(y, axis=1)
     true_false_array = tf.equal(y_pred, y)
     num_of_true = tf.reduce_sum(tf.cast(true_false_array, tf.int32))
     accuracy = num_of_true/x
-    mean_accuracy = tf.reduce_mean(accuracy, name="Mean",tf.float32)
+    mean_accuracy = tf.reduce_mean(accuracy, name="Mean", )
     return mean_accuracy
