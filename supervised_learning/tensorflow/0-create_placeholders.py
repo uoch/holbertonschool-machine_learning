4b1753210fe86007ae23ac2ad94e843a32b01b53
uoch <chaouch.thameurtc61@gmail.com>
2023-08-23 09:46:13 +0100

    tensor

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""tensorflow project"""
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+def create_placeholders(nx, classes):
+    """Create placeholders"""
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
+    return x, y
