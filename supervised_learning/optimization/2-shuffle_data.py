ffbcee726cd0168467ab945fbe7fc9d247ad3e6b
uoch <chaouch.thameurtc61@gmail.com>
2023-08-28 16:22:45 +0100

    norm

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -6,6 +6,6 @@ import numpy as np
 def shuffle_data(X, Y):
     """shuffles the data points in two matrices the same way"""
 
-    X_shuffled = np.random.permutation(X)
-    Y_shuffled = np.random.permutation(Y)
-    return X_shuffled, Y_shuffled
+    shuffler = [i for i in range(len(X))]
+    x = np.random.shuffle(shuffler)
+    return X[x], Y[x]
