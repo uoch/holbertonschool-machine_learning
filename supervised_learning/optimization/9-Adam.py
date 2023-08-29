619afee7be231addec620197bb1781d76d878844
uoch <chaouch.thameurtc61@gmail.com>
2023-08-29 11:43:40 +0100

    ss_vv

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -2,6 +2,7 @@
 """Normalization Constants"""""
 import numpy as np
 
+
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """
     alpha is the learning rate
@@ -13,10 +14,9 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     v is the previous first moment of var
     s is the previous second moment of var
     t is the time step used for bias correction
-    Returns: the updated variable, the new first moment, and the new second moment, respectively
     """
-    ss= beta2 * s + (1 - beta2) * (grad ** 2)
-    vv= beta1 * v + (1 - beta1) * grad
+    ss = beta2 * s + (1 - beta2) * (grad ** 2)
+    vv = beta1 * v + (1 - beta1) * grad
     ss_corrected = ss / (1 - beta2 ** t)
     vv_corrected = vv / (1 - beta1 ** t)
     var = var - (alpha * (vv_corrected / (np.sqrt(ss_corrected) + epsilon)))
