893fcb9cea79743ece8f44ce44b89ea0bb7f5a3b
uoch <chaouch.thameurtc61@gmail.com>
2023-09-02 09:50:10 +0100

    stuck

--- supervised_learning/optimization/15-model.py
+++ supervised_learning/optimization/15-model.py
@@ -36,10 +36,8 @@ def create_batch_norm_layer(prev, n, activation):
         return z
     else:
         mean, variance = tf.nn.moments(z, axes=[0])
-        gamma = tf.Variable(initial_value=tf.constant(1.0, shape=[n]),
-                            name='gamma', trainable=True)
-        beta = tf.Variable(initial_value=tf.constant(
-            0.0, shape=[n]), name='beta', trainable=True)
+        gamma = tf.Variable(1., trainable=True)
+        beta = tf.Variable(0., trainable=True)
         epsilon = 1e-8
         z_norm = tf.nn.batch_normalization(
             z, mean, variance, beta, gamma, epsilon)
@@ -76,7 +74,7 @@ def create_Adam_op(loss, alpha, beta1, beta2, epsilon, global_step):
     epsilon is a small number to avoid division by zero
     """
     op = tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon)
-    return op.minimize(loss, global_step=global_step)
+    return op.minimize(loss, global_step)
 
 
 def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
@@ -90,22 +88,14 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
     tf.add_to_collection('x', x)
     y = tf.placeholder(tf.float32, shape=[None, Y_train.shape[1]], name='y')
     tf.add_to_collection('y', y)
-
     y_pred = forward_prop(x, layers, activations, epsilon)
     tf.add_to_collection('y_pred', y_pred)
-
     loss = calculate_loss(y, y_pred)
     tf.add_to_collection('loss', loss)
-
     accuracy = calculate_accuracy(y, y_pred)
     tf.add_to_collection('accuracy', accuracy)
-
     global_step = tf.Variable(0, trainable=False)
-
-    decay_step = len(X_train) // batch_size
-    if (len(X_train) % batch_size):
-        decay_step += 1
-
+    decay_step = (len(X_train) // batch_size)//3.5
     alpha_decay = learning_rate_decay(
         alpha, decay_rate, global_step, decay_step)
     train_op = create_Adam_op(loss, alpha_decay, beta1,
@@ -117,44 +107,41 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
     with tf.Session() as sess:
         sess.run(init)
         for i in range(epochs):
+            count = 1
             cost, train_acc = sess.run((loss, accuracy), feed_dict={
                                        x: X_train, y: Y_train})
-
             valid_cost, valid_acc = sess.run(
                 (loss, accuracy), feed_dict={x: X_valid, y: Y_valid})
-
             print("After {} epochs:".format(i))
             print("\tTraining Cost: {}".format(cost))
             print("\tTraining Accuracy: {}".format(train_acc))
             print("\tValidation Cost: {}".format(valid_cost))
             print("\tValidation Accuracy: {}".format(valid_acc))
-
             X_sh, Y_sh = shuffle_data(X_train, Y_train)
             for j in range(0, X_train.shape[0], batch_size):
-                count = (j // batch_size) + 1
-                X_bat = X_sh[j:j+batch_size, :]  # conserve all the columns
-                Y_bat = Y_sh[j:j+batch_size, :]  # conserve all the columns
+                if len(X_train) % batch_size:
+                    X_bat = X_sh[j:j+batch_size, :]  # conserve all the columns
+                    Y_bat = Y_sh[j:j+batch_size, :]  # conserve all the columns
+                else:
+                    X_bat = X_sh[j:(j+batch_size+1):]
+                    Y_bat = Y_sh[j:(j+batch_size+1), :]
                 sess.run(train_op, feed_dict={
-                    x: X_bat, y: Y_bat, global_step: count})
-
-                if not (count % 100):
-
+                         x: X_bat, y: Y_bat, global_step: i})
+                if count % 100 == 0:
                     batch_cost, batch_acc = sess.run(
                         (loss, accuracy), feed_dict={x: X_bat, y: Y_bat})
                     print("\tStep {}:".format(count))
                     print("\t\tCost: {}".format(batch_cost))
                     print("\t\tAccuracy: {}".format(batch_acc))
+                count += 1
         cost_f, train_acc_f = sess.run((loss, accuracy), feed_dict={
                                        x: X_train, y: Y_train})
-
         valid_cost_f, valid_acc_f = sess.run(
             (loss, accuracy), feed_dict={x: X_valid, y: Y_valid})
-
         print("After {} epochs:".format(epochs))
         print("\tTraining Cost: {}".format(cost_f))
         print("\tTraining Accuracy: {}".format(train_acc_f))
         print("\tValidation Cost: {}".format(valid_cost_f))
         print("\tValidation Accuracy: {}".format(valid_acc_f))
-
         save_path = saver.save(sess, save_path)
     return save_path
