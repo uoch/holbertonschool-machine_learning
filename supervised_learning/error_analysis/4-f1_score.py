18762c90aaaee431e6f2965fab870f3fd19e22f6
uoch <chaouch.thameurtc61@gmail.com>
2023-09-04 16:27:52 +0100

    tn

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """error analysis"""
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
