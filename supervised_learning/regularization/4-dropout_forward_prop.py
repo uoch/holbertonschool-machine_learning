7c144924e466480290b2669113bdea36277aa6e6
uoch <chaouch.thameurtc61@gmail.com>
2023-09-05 14:31:03 +0100

    should_div_with_keep_prob

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -4,16 +4,19 @@ import numpy as np
 
 
 def sigmoid(z):
+    """sigmoid activation function"""
     a = 1.0/(1.0+np.exp(-z))
     return a
 
 
 def tanh(z):
+    """tanh activation function"""
     a = 2 * sigmoid(2 * z) - 1
     return a
 
 
 def softmax(z):
+    """softmax activation function"""
     z_exp = np.exp(z - np.max(z, axis=0, keepdims=True))
     a = z_exp / np.sum(z_exp, axis=0, keepdims=True)
     return a
@@ -24,13 +27,12 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     # random.binomial(n, p, size=None)
     a = {}
     a['A0'] = X
-    li = []
     for i in range(L):
         z = np.matmul(weights['W' + str(i + 1)], a['A' + str(i)])\
             + weights['b' + str(i + 1)]
         a['D' + str(i + 1)] = np.random.binomial(1, keep_prob, size=z.shape)
         if i == L - 1:
-            a['A' + str(i + 1)] = softmax(z) * a['D' + str(i + 1)]
+            a['A' + str(i + 1)] = softmax(z) * a['D' + str(i + 1)]/keep_prob
         else:
-            a['A' + str(i + 1)] = tanh(z) * a['D' + str(i + 1)]
+            a['A' + str(i + 1)] = tanh(z) * a['D' + str(i + 1)]/keep_prob
     return a
