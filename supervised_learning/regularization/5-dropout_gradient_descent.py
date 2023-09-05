6b71ee7991987bc4708406c7337477794357cc70
uoch <chaouch.thameurtc61@gmail.com>
2023-09-05 16:19:47 +0100

    apply_on_dz

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -11,7 +11,7 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     for i in range(L, 0, -1):
         if i == L:
             dz = cache['A' + str(L)] - Y
-        ap = cache['A' + str(i - 1)] * cache['D' + str(i - 1)]
+        ap = cache['A' + str(i - 1)]
         dw = np.matmul(dz, ap.T) / m
         db = np.sum(dz, axis=1, keepdims=True) / m
         dz = ((np.matmul(weights['W' + str(i)].T, dz)
