8c2af390f42ae956b2f5b709a08c86d2dc427dba
uoch <chaouch.thameurtc61@gmail.com>
2023-09-11 15:19:43 +0100

    .fit

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -3,7 +3,8 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size,
+                epochs, verbose=True, shuffle=False):
     """Train a model with Keras and return the History object"""
 
     return network.fit(data, labels, batch_size=batch_size, epochs=epochs,
