de195ad03b6aab8212fd901b79cd0d0d9cf720d8
uoch <chaouch.thameurtc61@gmail.com>
2023-09-11 10:45:41 +0100

    drop

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -7,13 +7,14 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """builds a neural network with the Keras library"""
     model = K.Sequential()
     L2 = K.regularizers.l2(lambtha)
+    dropout = K.layers.Dropout(1-keep_prob)
     for i in range(len(layers)):
         if i == 0:
             model.add(K.layers.Dense(layers[i], input_shape=(nx,),
                                      activation=activations[i],
                                      kernel_regularizer=L2))
         model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                 kernel_regularizer=L2))
+                                     kernel_regularizer=L2))
         if i != len(layers) - 1:
-            model.add(K.layers.Dropout(1 - keep_prob))
+            model.add(dropout)
     return model
