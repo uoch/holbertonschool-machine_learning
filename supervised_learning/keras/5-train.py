3b588a04be1c703b94d31da21fd9c09c30f58428
uoch <chaouch.thameurtc61@gmail.com>
2023-09-11 15:24:20 +0100

    .fit

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""One-hot encode with Keras"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """Train a model with Keras and return the History object"""
+
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle,
+                       validation_data=validation_data)
