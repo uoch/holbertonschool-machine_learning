4ca66b5ad574a37ac6ad826168fc5bd7bd4eaeba
uoch <chaouch.thameurtc61@gmail.com>
2023-09-12 17:08:59 +0100

    no_padding

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """convolve_grayscale_valid"""
 import numpy as np
-import
 
 
 def convolve_grayscale_valid(images, kernel):
     """cnvolve_grayscale_valid"""
-    m , h, w = images.shape
+    m, h, w = images.shape
     kh, kw = kernel.shape
-    output_h = h - kh + 1 # padding = 0
-    output_w = w - kw + 1 # padding = 0
+    output_h = h - kh + 1  # padding = 0
+    output_w = w - kw + 1  # padding = 0
     output = np.zeros((m, output_h, output_w))
     for i in range(output_h):
         for j in range(output_w):
