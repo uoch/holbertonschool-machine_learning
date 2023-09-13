1c5c401ed11f3ddb24167a0c676500510c46c360
uoch <chaouch.thameurtc61@gmail.com>
2023-09-13 12:14:20 +0100

    all_together

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -17,38 +17,48 @@ def adjust_kernel(kernel):
     return ph, pw
 
 
+def w_h_out(w_prev, h_prev, kh, kw, sh, sw, padding, kernel):
+    if padding == 'same':
+        ph, pw = adjust_kernel(kernel)
+        output_h = (h_prev + 2 * ph - kh) // sh + 1
+        output_w = (w_prev + 2 * pw - kw) // sw + 1
+    if padding == 'valid':
+        output_h = (h_prev - kh) // sh + 1
+        output_w = (w_prev - kw) // sw + 1
+    if type(padding) == tuple:
+        ph, pw = padding
+        output_h = (h_prev + 2 * ph - kh) // sh + 1
+        output_w = (w_prev + 2 * pw - kw) // sw + 1
+    return output_h, output_w
+
+
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
-    """cnvolve_grayscale_same"""
     m, h, w = images.shape
     kh, kw = kernel.shape
+    i_step, j_step = stride
 
     if padding == 'same':
         p_h, p_w = adjust_kernel(kernel)
         padded_images = np.pad(
             images, ((0, 0), (p_h, p_h), (p_w, p_w)),
             mode='constant', constant_values=0)
-
     elif padding == 'valid':
         p_h, p_w = (0, 0)
         padded_images = images
 
-    if type(padding) == tuple:
+    elif type(padding) == tuple:
         p_h, p_w = padding
         padded_images = np.pad(
             images, ((0, 0), (p_h, p_h), (p_w, p_w)),
             mode='constant', constant_values=0)
-    if padding == 'same':
-        output_h = h
-        output_w = w
-    else:
-        output_h = h + 2 * p_h - kh + 1
-        output_w = w + 2 * p_w - kw + 1
+
+    output_h, output_w = w_h_out(w, h, kh, kw, i_step, j_step, padding, kernel)
     output = np.zeros((m, output_h, output_w))
-    i_step, j_step = stride
     for i in range(0, output_h, i_step):
         for j in range(0, output_w, j_step):
             zoom_in = padded_images[:, i:i+kh, j:j+kw]
             product = kernel * zoom_in
             pixel = np.sum(product, axis=(1, 2))
             output[:, i, j] = pixel
+
     return output
