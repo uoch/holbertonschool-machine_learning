f108351654ce17442d5138b71a97f458534cdd67
uoch <chaouch.thameurtc61@gmail.com>
2023-09-14 10:08:52 +0100

    pooling

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -11,17 +11,17 @@ def pool(images, kernel_shape, stride, mode='max'):
 
     output_h = int(np.ceil((h-kh)/i_step+1))
     output_w = int(np.ceil((w-kw)/j_step+1))
-    output = np.zeros((m, output_h, output_w))
+    output = np.zeros((m, output_h, output_w, c))
     for i in range(0, output_h):
         x = i*i_step  # you should use the step on the image, not the output
         for j in range(0, output_w):
             y = j*j_step  # you should use step on the image, not the output
             zoom_in = images[:, x:x+kh, y:y+kw, :]
             if mode == 'max':
-                pixel = np.max(zoom_in, axis=(1, 2, 3))
-                output[:, i, j] = pixel
+                pixel = np.max(zoom_in)
+                output[:, i, j, :] = pixel
             elif mode == 'avg':
-                pixel = np.mean(zoom_in, axis=(1, 2, 3))
-                output[:, i, j] = pixel
+                pixel = np.mean(zoom_in)
+                output[:, i, j, :] = pixel
 
     return output
