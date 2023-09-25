d538ef8e222f9a5efe4a1ba48366ede4ea031beb
uoch <chaouch.thameurtc61@gmail.com>
2023-09-25 17:22:58 +0100

    done

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -19,14 +19,14 @@ def inception_network():
     # enter inception blocks
     x6 = inception_block(x5, [64, 96, 128, 16, 32, 32])
     x7 = inception_block(x6, [128, 128, 192, 32, 96, 64])
-    x8 = K.layers.AveragePooling2D(pool_size=(7, 7), strides=(1, 1))(x7)
+    x8 = K.layers.MaxPool2D((3, 3), 2, padding='same')(x7)
     # back to inception blocks
     x9 = inception_block(x8, [192, 96, 208, 16, 48, 64])
     x10 = inception_block(x9, [160, 112, 224, 24, 64, 64])
     x11 = inception_block(x10, [128, 128, 256, 24, 64, 64])
     x12 = inception_block(x11, [112, 144, 288, 32, 64, 64])
     x13 = inception_block(x12, [256, 160, 320, 32, 128, 128])
-    union = K.layers.MaxPool2D(pool_size=(7, 7), strides=(1, 1))(x13)
+    union = K.layers.MaxPooling2D((3, 3), 2, padding='same')(x13)
     # back to inception blocks
     x14 = inception_block(union, [256, 160, 320, 32, 128, 128])
     x15 = inception_block(x14, [384, 192, 384, 48, 128, 128])
@@ -34,6 +34,6 @@ def inception_network():
         pool_size=(7, 7), strides=(1, 1))(x15)
     y = K.layers.Dropout(0.4)(final_union)
     # linear softmax for the final layer
-    x9 = K.layers.Dense(units=1000, activation='softmax')(y)
-    model = K.Model(inputs=X, outputs=x9)
+    Y = K.layers.Dense(units=1000, activation='softmax')(y)
+    model = K.Model(inputs=X, outputs=Y)
     return model
