832603ac284bd06e0307f8dfef11fc9a962f2209
uoch <chaouch.thameurtc61@gmail.com>
2023-09-27 10:35:13 +0100

    connection_to_the_blocks

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -12,7 +12,7 @@ def transition_layer(X, nb_filters, compression):
     batch1 = K.layers.BatchNormalization(axis=3)(X)
     activation1 = K.layers.Activation('relu')(batch1)
     conv1 = K.layers.Conv2D(filters=comp,
-                            kernel_size=(1, 1), 
+                            kernel_size=(1, 1),
                             padding='same',
                             kernel_initializer='he_normal')(activation1)
     avg_pool = K.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2),
