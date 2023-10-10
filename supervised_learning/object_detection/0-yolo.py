55fe530670b58272addbf43d4d5107f3de6669ad
uoch <chaouch.thameurtc61@gmail.com>
2023-10-10 08:33:55 +0100

    yolo

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -10,6 +10,11 @@ class Yolo:
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """Constructor method
         """
+        if not os.path.exists(model_path):
+            raise FileNotFoundError("Wrong model file path")
+
+        if not os.path.exists(classes_path):
+            raise FileNotFoundError("Wrong classes file path")
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
             self.class_names = [line[:-1] for line in f]
