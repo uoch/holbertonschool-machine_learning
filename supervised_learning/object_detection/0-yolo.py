0375c105ceb118970f16725c635798e9eb1545b9
uoch <chaouch.thameurtc61@gmail.com>
2023-10-10 08:38:58 +0100

    yolo

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -8,6 +8,14 @@ import os
 class Yolo:
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """Constructor method
+        model_path: path to where a Darknet Keras model is stored
+        classes_path: path to where the list of class names used for the
+        Darknet model, listed in order of index, can be found
+        class_t: float representing the box score threshold for the initial
+        filtering step
+        nms_t: float representing the IOU threshold for non-max suppression
+        anchors: numpy.ndarray of shape (outputs, anchor_boxes, 2) containing
+        all of the anchor boxes:
         """
         if not os.path.exists(model_path):
             raise FileNotFoundError("Wrong model file path")
