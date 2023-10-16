b0547b730ee16c5ccdc50f1cca1cd91ede4012c9
uoch <chaouch.thameurtc61@gmail.com>
2023-10-16 11:34:17 +0100

    nst

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -12,14 +12,12 @@ class NST:
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
         """initialization"""
         tf.executing_eagerly()
-        if type(style_image) is not np.ndarray or \
-                style_image.ndim != 3 or style_image.shape[2] != 3:
+        if type(style_image) is not np.ndarray or style_image.shape[2] != 3:
             raise TypeError(
-                'style_image must be a numpy.ndarray with shape (h, w, 3)')
-        if type(content_image) is not np.ndarray or\
-                content_image.ndim != 3 or content_image.shape[2] != 3:
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
+        if type(content_image) is not np.ndarray or content_image.shape[2] != 3:
             raise TypeError(
-                'content_image must be a numpy.ndarray with shape (h, w, 3)')
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
         if (type(alpha) is not int and type(alpha) is not float) or alpha < 0:
             raise TypeError('alpha must be a non-negative number')
         if (type(beta) is not int and type(beta) is not float) or beta < 0:
@@ -35,13 +33,13 @@ class NST:
           between 0 and 1 and its largest side is 512 pixels
           """
         if type(image) is not np.ndarray or\
-                image.ndim != 3 or image.shape[2] != 3:
+                image.shape[2] != 3:
             raise TypeError(
                 'content_image must be a numpy.ndarray with shape (h, w, 3)')
         h, w, c = image.shape
         maxd = max(h, w)
-        new_h = int(h * (512/maxd))
-        new_w = int(w * (512/maxd))
+        new_h = round(h * (512/maxd))
+        new_w = round(w * (512/maxd))
         image = tf.expand_dims(image, axis=0)
         image /= 255
         resized = tf.image.resize(image, size=(new_h, new_w), method="bicubic")
