59789814daa9ff0e45e7b67c4d747d3c34686216
uoch <chaouch.thameurtc61@gmail.com>
2023-11-02 15:17:53 +0100

    init

---
+++ math/multivariate_prob/multinormal.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""multivariate normal distribution"""
+import numpy as np
+
+
+class MultiNormal:
+    """class that represents a Multivariate Normal distribution"""
+
+    def __init__(self, data):
+        """Constructor"""
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
+            raise TypeError('data must be a 2D numpy.ndarray')
+        if data.shape[1] < 2:
+            raise ValueError('data must contain multiple data points')
+        data = data.T
+        self.mean = np.mean(data, axis=0, keepdims=True)
+        data = data - self.mean
+        self.cov = np.matmul(data.T, data) / (data.shape[0] - 1)
