c05e944320b155be5bd2fea3da39ea789df27d4f
uoch <chaouch.thameurtc61@gmail.com>
2023-11-07 09:49:12 +0100

    88

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
-"""bayesian prob"""
+"""Intersection Module"""
 import numpy as np
+likelihood = __import__('0-likelihood').likelihood
 
 
 def intersection(x, n, P, Pr):
-    """ calculates the intersection of P and Pr
-    x = num of patients that develop severe side effects
-    n = total num of patients observed
-    P = 1D np.ndarray containing the various hypothetical probabilities
-    of developing severe side effects
-    Pr = 1D np.ndarray containing the prior beliefs of P
-    p.shape = (n,) where n is the number of patients"""
+    """Calculates the intersection of obtaining this data with the various
+    hypothetical probabilities:
+    """
     if not isinstance(n, int) or n <= 0:
-        raise ValueError('n must be a positive integer')
+        raise ValueError("n must be a positive integer")
+
     if not isinstance(x, int) or x < 0:
         raise ValueError(
-            'x must be an integer that is greater than or equal to 0')
+            "x must be an integer that is greater than or equal to 0")
+
     if x > n:
-        raise ValueError('x cannot be greater than n')
+        raise ValueError("x cannot be greater than n")
+
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
-        raise TypeError('P must be a 1D numpy.ndarray')
+        raise TypeError("P must be a 1D numpy.ndarray")
+
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
-        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
-    if np.any(P > 1) or np.any(P < 0):
-        raise ValueError(f'All values in {P} must be in the range [0, 1]')
-    if np.isclose(np.sum(Pr), 1) is False:
-        raise ValueError('Pr must sum to 1')
-    # pmf is the probability mass function of a binomial dist for n and x
-    # likelihood = pmf * (P ** x) * ((1 - P) ** (n - x))
-    # intersection = likelihood * Pr
-    pmf = np.math.factorial(n) / (np.math.factorial(x)
-                                  * np.math.factorial(n - x))
-    likelihood = pmf * (P ** x) * ((1 - P) ** (n - x))
-    intersection = likelihood * Pr
-    return intersection
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    if not all(0 <= p <= 1 for p in P):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if not all(0 <= pr <= 1 for pr in Pr):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    return likelihood(x, n, P) * Pr
