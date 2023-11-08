ee569b770ba4edb7e997ab6c530489be3917d762
uoch <chaouch.thameurtc61@gmail.com>
2023-11-08 12:18:12 +0100

    weigts

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""dimensionality reduction"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """performs PCA on a dataset
+    X ndarray (n, d) dataset
+    var fraction of variance that PCA transformation should maintain"""
+    n,d = X.shape
+    u, s, vh = np.linalg.svd(X)
+    weights_matrix = vh.T
+    # determine number of components to keep
+    k = 0
+    r = 0
+    for i in range(len(s)):
+        k += s[i]
+        r += 1
+        if k >= var:
+            r +=1
+            break
+    return weights_matrix[:, :r+1]
