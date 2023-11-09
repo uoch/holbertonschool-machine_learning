50cdd7d1d63984408b2d1667393873f1457ba7f6
uoch <chaouch.thameurtc61@gmail.com>
2023-11-09 13:07:19 +0100

    grad

--- unsupervised_learning/dimensionality_reduction/6-grads.py
+++ unsupervised_learning/dimensionality_reduction/6-grads.py
@@ -7,16 +7,10 @@ Q_affinities = __import__('5-Q_affinities').Q_affinities
 def grads(Y, P):
     """calculates the gradients of Y"""
     Q, num = Q_affinities(Y)
-
     # (pi j − qi j )
     PQ = P - Q
-
     # (yi − yj)
     Y_diff = Y[:, np.newaxis, :] - Y
-
-    dY = np.zeros(Y.shape)
-    for i in range(Y.shape[0]):
-        # Use element-wise multiplication and sum along the appropriate axis
-        dY[i] = np.sum((PQ * num)[:, i, np.newaxis] * Y_diff[:, i, :], axis=0)
+    dY = -np.sum((PQ * num)[:, :, np.newaxis] * Y_diff[:, :, :], axis=0)
 
     return -dY, Q
