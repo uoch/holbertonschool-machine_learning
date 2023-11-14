8c4e29c5d86142edde7cf66f7279e83a90ab7ee2
uoch <chaouch.thameurtc61@gmail.com>
2023-11-14 13:55:02 +0100

    int

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def initialize(X, k):
     """initializes cluster centroids for K-means"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
     n, d = X.shape
     min = np.amin(X, axis=0)
     max = np.amax(X, axis=0)
