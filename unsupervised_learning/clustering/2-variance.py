f780024f4f324b28ed846eca6eea0b724fec63b8
uoch <chaouch.thameurtc61@gmail.com>
2023-11-14 15:42:32 +0100

    var

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -5,6 +5,9 @@ import numpy as np
 
 def variance(X, C):
     """Calculate intra-cluster variance for a data set"""
-
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None
+    if not isinstance(C, np.ndarray) or C.ndim != 2:
+        return None
     distances = np.linalg.norm(X - C[:, np.newaxis], axis=-1) ** 2
     return np.sum(np.min(distances, axis=0))
