880b95741121a023321d7ae7b56f2f4604f18a75
uoch <chaouch.thameurtc61@gmail.com>
2023-11-16 14:27:11 +0100

    ski

---
+++ unsupervised_learning/clustering/11-gmm.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""clustering"""
+import sklearn.cluster
+
+
+def gmm(X,k):
+    """performs K-means on a dataset"""
+    gmm = sklearn.mixture.GaussianMixture(n_components=k).fit(X)
+    pi = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+    clss = gmm.predict(X)
+    bic = gmm.bic(X)
+    return pi, m, S, clss, bic
