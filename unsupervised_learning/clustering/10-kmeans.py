af56b391a3568db9341a018a4e8f6d2669ccd2d2
uoch <chaouch.thameurtc61@gmail.com>
2023-11-16 14:22:14 +0100

    ski

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """performs K-means on a dataset"""
+    kmeans = sklearn.cluster.KMeans(k).fit(X)
+    return kmeans.cluster_centers_, kmeans.labels_
