2912bfdd791b97a14a3dbfcd822011a3a42afd06
uoch <chaouch.thameurtc61@gmail.com>
2023-12-07 09:35:18 +0100

    var

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -1,39 +1,11 @@
 #!/usr/bin/env python3
 """autoencoder"""
 import tensorflow.keras as K
-import tensorflow as tf
-
-
-class VAE_LossLayer(K.layers.Layer):
-    """loss layer for variational autoencoder"""
-
-    def __init__(self, **kwargs):
-        """initialization function"""
-        super(VAE_LossLayer, self).__init__(**kwargs)
-
-    def call(self, inputs):
-        """implements CVAE loss
-        Args:
-            inputs: list containing
-                    [inputs, outputs, latent_mean, latent_log_var]"""
-        inputs, outputs, mu, var = inputs
-
-        # Reconstruction loss
-        reconstruction_loss = tf.keras.losses.binary_crossentropy(
-            inputs, outputs)
-
-        # KL divergence loss
-        kl_loss = -0.5 * tf.reduce_sum(1 + mu -
-                                       tf.square(mu) - tf.exp(var), axis=-1)
-
-        # Total VAE loss
-        loss = tf.reduce_mean(reconstruction_loss + kl_loss)
-        self.add_loss(loss)
-        return loss
+import tensorflow.keras as K
 
 
 def create_model(input_dim, layers, latent, d=True):
-    """basic encoder model"""
+    """Basic encoder model"""
     inputs = K.Input(shape=(input_dim,))
     for i, layer in enumerate(layers):
         if layer == layers[0]:
@@ -49,16 +21,26 @@ def create_model(input_dim, layers, latent, d=True):
 
 
 def gaussian_mean_and_log_variance(output, latent_dims):
-    """returns the mean and log variance"""
+    """Returns the mean and log variance"""
     mean = K.layers.Dense(latent_dims)(output)
     log_var = K.layers.Dense(latent_dims)(output)
     return mean, log_var
 
 
 def sampling(mean, log_var):
-    """samples from the standard normal distribution"""
+    """Samples from the standard normal distribution"""
     epsilon = K.backend.random_normal(shape=K.backend.shape(mean))
     return mean + K.backend.exp(log_var / 2) * epsilon
+
+
+def loss(inputs, latent_output, latent_dims):
+    """Loss function"""
+    mu, log_var = gaussian_mean_and_log_variance(latent_output, latent_dims)
+    mse = K.losses.binary_crossentropy(inputs, latent_output)
+    kl = -0.5 * K.backend.sum(1 + log_var -
+                              K.backend.square(mu) - K.backend.exp(log_var), axis=-1)
+    loss = K.backend.mean(mse + kl)
+    return loss
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
@@ -78,14 +60,15 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
 
     # Combined autoencoder model
     autoencoder_input = K.Input(shape=(input_dims,))
-    latent_mean, latent_log_var, latent_z = encoder_model(autoencoder_input)
+    _, _, latent_z = encoder_model(autoencoder_input)
     reconstructed_output = decoder_model(latent_z)
-    loss_layer = VAE_LossLayer()(
-        [autoencoder_input, reconstructed_output, latent_mean, latent_log_var])
+
+    # Loss calculation
+    loss_value = loss(autoencoder_input, reconstructed_output, latent_dims)
 
     # Combined model with VAE loss
     autoencoder_model = K.Model(autoencoder_input, reconstructed_output)
-    autoencoder_model.add_loss(loss_layer)
+    autoencoder_model.add_loss(loss_value)
     autoencoder_model.compile(optimizer='adam', loss=None)
 
     return encoder_model, decoder_model, autoencoder_model
