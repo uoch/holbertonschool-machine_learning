310943726360c69c1f845e8599b5b865843a54c8
uoch <chaouch.thameurtc61@gmail.com>
2023-12-27 12:35:29 +0100

    bag

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -3,20 +3,39 @@
 import numpy as np
 
 
+def clean_string(word):
+    """cleans a string"""
+    word = word.lower()
+    word = word.strip()
+    word = word.strip('.')
+    word = word.strip('!')
+    word = word.strip('?')
+    word = word.strip(',')
+    word = word.strip(':')
+    word = word.strip(';')
+    return word
+
+
 def bag_of_words(sentences, vocab=None):
     """transforms sentences to a bag of words embedding matrix"""
     if vocab is None:
-        vocab = set(word for sentence in sentences for word in sentence.split())
+        vocab = set(clean_string(word)
+                    for sent in sentences for word in sent.split())
 
     features = list(vocab)
     s = len(sentences)
     f = len(features)
 
-    embeddings = np.zeros((s, f))
+    # Initialize the embeddings matrix
+    embeddings = np.zeros((s, f), dtype=int)
 
+    # Loop through sentences and words to fill the embeddings matrix
     for i, sentence in enumerate(sentences):
         words = sentence.split()
-        for j, word in enumerate(features):
-            embeddings[i][j] = words.count(word)
+        for j, word in enumerate(words):
+            words[j] = clean_string(word)
+            if words[j] in vocab:
+                word_idx = features.index(words[j])
+                embeddings[i][word_idx] += 1
 
     return embeddings, sorted(features)
