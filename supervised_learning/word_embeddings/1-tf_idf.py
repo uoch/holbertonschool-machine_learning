bd3a0670ac7c65d8f94846f65b26bc5483398ad6
uoch <chaouch.thameurtc61@gmail.com>
2024-01-02 09:00:54 +0100

    tf

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""0. Bag Of Words"""
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tf_idf(sentences, vocab=None):
+    """creates a TF-IDF embedding"""
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    return X.toarray(), vectorizer.get_feature_names_out()
