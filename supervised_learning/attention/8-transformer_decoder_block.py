9bee8225acb3203c871012b29b569d3f53f5c959
uoch <chaouch.thameurtc61@gmail.com>
2024-01-10 11:40:47 +0100

    decode

--- supervised_learning/attention/8-transformer_decoder_block.py
+++ supervised_learning/attention/8-transformer_decoder_block.py
@@ -12,11 +12,14 @@ class DecoderBlock(tf.keras.layers.Layer):
         super(DecoderBlock, self).__init__()
         self.mha1 = MultiHeadAttention(dm, h)
         self.mha2 = MultiHeadAttention(dm, h)
+
         self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
         self.dense_output = tf.keras.layers.Dense(dm)
+
         self.dropout1 = tf.keras.layers.Dropout(drop_rate)
         self.dropout2 = tf.keras.layers.Dropout(drop_rate)
         self.dropout3 = tf.keras.layers.Dropout(drop_rate)
+
         self.layer_norm1 = tf.keras.layers.LayerNormalization(
             epsilon=1e-6)
         self.layer_norm2 = tf.keras.layers.LayerNormalization(
@@ -28,13 +31,13 @@ class DecoderBlock(tf.keras.layers.Layer):
         """call function"""
         # masked multi-head attention block
         attn1, _ = self.mha1(x, x, x, look_ahead_mask)
-        attn1 = self.dropout1(attn1, training=training)
+        attn1 = self.dropout1(attn1)
         # add and norm block
         out1 = self.layer_norm1(x + attn1)
         # multi-head attention block linked to encoder output
         attn2, _ = self.mha2(
             out1, enc_output, enc_output, padding_mask)
-        attn2 = self.dropout2(attn2, training=training)
+        attn2 = self.dropout2(attn2)
         # add and norm block
         out2 = self.layer_norm2(out1 + attn2)
         # feed forward block (dense)
@@ -43,7 +46,7 @@ class DecoderBlock(tf.keras.layers.Layer):
             self.dense_output
         ])
         ffn_output = ffn(out2)
-        ffn_output = self.dropout3(ffn_output, training=training)
+        ffn_output = self.dropout3(ffn_output)
         # add and norm block
         output = self.layer_norm3(out2 + ffn_output)
         return output
