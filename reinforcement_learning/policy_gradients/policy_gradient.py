61e65e3e3b3ef8c43e1e17e5e62203a31b2a31cc
chaouch <chaouch.thameurtc61@gmail.com>
2024-03-06 13:49:28 +0100

    done

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -1,11 +1,23 @@
-#!/usr/bin/env python3
-"""policy gradient"""
 import numpy as np
 
 
 def softmax(logits):
-    """softmax function"""
-    return np.exp(logits) / np.sum(np.exp(logits), keepdims=True)
+    """softmax function with normalization"""
+    # Normalize logits for numerical stability
+    normalized_logits = logits - np.max(logits)
+    exp_logits = np.exp(normalized_logits)
+    return exp_logits / np.sum(exp_logits)
+
+
+def softmax_grad(s):
+    jacobian_m = np.diag(s)
+    for i in range(len(jacobian_m)):
+        for j in range(len(jacobian_m)):
+            if i == j:
+                jacobian_m[i][j] = s[i] * (1-s[i])
+            else:
+                jacobian_m[i][j] = -s[i] * s[j]
+    return jacobian_m
 
 
 def policy(state, weight):
@@ -13,16 +25,18 @@ def policy(state, weight):
     # flatten the state with the weight
     logits = np.dot(state, weight)
     action_probs = softmax(logits)
+    if np.isnan(action_probs).any():
+        return np.ones_like(action_probs) / len(action_probs)
     return action_probs
 
 
 def policy_gradient(state, weight):
-    """Simple policy function"""
-    # Flatten the state with the weight
-    action_probs = policy(state, weight)
+    logits = np.dot(state, weight)
+    action_probs = softmax(logits)
     action = np.random.choice(len(action_probs), p=action_probs)
-    I = np.eye(len(action_probs))
-    gradient = np.outer(state.T, action_probs) @ (I -
-                                                  np.outer(action_probs, action_probs))
+    if np.isnan(action_probs).any():
+        return np.ones_like(action_probs) / len(action_probs)
+    grad_log_probs = np.outer(
+        state, (action_probs - (action == np.arange(len(action_probs)))))
 
-    return action, gradient
+    return action, grad_log_probs
--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -20,20 +20,39 @@ def he_init(shape):
     stddev = np.sqrt(2.0 / fan_in)
     return np.random.normal(0, stddev, shape)
 
-def train(env, nb_episodes, alpha=0.000045, gamma=0.98 , show_result = False):
-    """training function"""
-    # initialize the weight
-    weight = he_init((env.observation_space.shape[0], env.action_space.n))
 
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98, epsilon=0.1, show_result=False, initial=he_init):
+    """Training function"""
+    # Initialize the weight
+    weight = initial((env.observation_space.shape[0], env.action_space.n))
+    mean_state = np.zeros(env.observation_space.shape[0])
+    std_state = np.ones(env.observation_space.shape[0])
     scores = []
+
     for ep in tqdm.tqdm(range(nb_episodes)):
-        state, _ = env.reset()
+        state ,_= env.reset()
         episode = []
         grads = []
         episode_score = 0
+
         while True:
-            action, grad = policy_gradient(state, weight)
-            next_state, reward, done, _, _ = env.step(action)
+            n_state = (state - mean_state) / std_state
+
+            # Epsilon-greedy exploration
+            if np.random.rand() < epsilon:
+                action = np.random.choice(env.action_space.n)
+                if np.isnan(action).any():
+                    action = np.ones_like(action) / len(action)
+                _, grad = policy_gradient(n_state, weight)
+            else:
+                action, grad = policy_gradient(n_state, weight)
+                if np.isnan(action).any():
+                    action = np.ones_like(action) / len(action)
+                action_probs = policy(state, weight)
+                if np.isnan(action_probs).any():
+                    action = np.ones_like(action_probs) / len(action_probs)
+
+            next_state, reward, done, _,_ = env.step(action)
             episode.append((state, action, reward))
             grads.append(grad)
             episode_score += reward
@@ -41,30 +60,42 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98 , show_result = False):
             state = next_state
             if done:
                 break
-        if show_result and (ep+1) % 1000 == 0:
+
+        states = np.array([step[0] for step in episode])
+        mean_state = np.mean(states, axis=0)
+        std_state = np.std(states, axis=0)
+
+        if show_result and (ep + 1) % 1000 == 0:
             env.render()
-            st ,_= env.reset()
+            st, _= env.reset()
             img = env.render()
             imgs = []
+
             while True:
                 action, _ = policy_gradient(st, weight)
-                state, reward, done, _, _ = env.step(action)
+                state, reward, done, _,_ = env.step(action)
                 img = env.render()
                 imgs.append(img)
                 if done:
                     break
-            gif_path = f'gif/cartpole{ep+1}.gif'
-            imageio.mimsave(gif_path, [np.array(img) for i, img in enumerate(imgs) ], fps=1000)
+
+            gif_path = f'gif/cartpole{ep + 1}.gif'
+            imageio.mimsave(gif_path, [np.array(img) for i, img in enumerate(imgs)], fps=1000)
             display.display(display.Image(filename=gif_path))
             time.sleep(1.5)  # Display the GIF for 2 seconds
             display.clear_output(wait=True)  # Clear the output after displaying the GIF
             env.close()
+
         scores.append(episode_score)
+
+        Gt = 0
         for i in range(len(episode)):
-            Gt = sum([gamma**j * episode[j][2]
-                     for j in range(i, len(episode))])
+            Gt = sum([gamma ** (j - i) * episode[j][2] for j in range(i, len(episode))])
             step, action, _ = episode[i]
             action_probs = policy(step, weight)
+            if np.isnan(action_probs).any():
+                action = np.ones_like(action_probs) / len(action_probs)
             Ln = np.log(action_probs[action])
-            weight += alpha * gamma**i * Gt * Ln * grads[i]
-    return scores 
+            weight += alpha * Gt * Ln * grads[i]
+
+    return scores, weight
