e6326d12719eb0bd940623ad6fecc337036eb9cc
chaouch <chaouch.thameurtc61@gmail.com>
2024-03-22 10:49:14 +0100

    Automated commit

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -2,39 +2,23 @@
 """api module"""
 import requests
 
-UPCOMING_URL = "https://api.spacexdata.com/v4/launches/upcoming"
-BASE_URL_ROCKET = "https://api.spacexdata.com/v4/rockets/"
-BASE_URL_LAUNCHPAD = "https://api.spacexdata.com/v4/launchpads/"
 
+if __name__ == '__main__':
+    launches = requests.get(
+        'https://api.spacexdata.com/v4/launches/upcoming').json()
+    unix_dates = [launch['date_unix'] for launch in launches]
+    min_idx = unix_dates.index(min(unix_dates))
+    upcoming_launch = launches[min_idx]
 
-def get_next_launch(api_response):
-    """get first launch from api response"""
-    dates = [x["date_unix"] for x in api_response]
-    index = dates.index(min(dates))
-    return response[index]
+    rocket = requests.get(
+        'https://api.spacexdata.com/v4/rockets/{}'.format(
+            upcoming_launch['rocket'])).json()
+    launchpad = requests.get(
+        'https://api.spacexdata.com/v4/launchpads/{}'.format(
+            upcoming_launch['launchpad'])).json()
 
-
-if __name__ == "__main__":
-    response = requests.get(UPCOMING_URL).json()
-
-    next_launch = get_next_launch(response)
-
-    name = next_launch.get("name")
-    date = next_launch.get("date_local")
-    rocket_id = next_launch.get("rocket")
-    launchpad_id = next_launch.get("launchpad")
-
-    response_rocket = requests.get(BASE_URL_ROCKET + rocket_id).json()
-    rocket_name = response_rocket["name"]
-
-    response_launchpad = requests.get(BASE_URL_LAUNCHPAD + launchpad_id).json()
-    launchpad_name = response_launchpad["name"]
-    launchpad_loc = response_launchpad["locality"]
-
-    print(
-        "{} ({}) {} - {} ({})".format(name,
-                                      date,
-                                      rocket_name,
-                                      launchpad_name,
-                                      launchpad_loc)
-    )
+    print('{} ({}) {} - {} ({})'.format(upcoming_launch['name'],
+                                        upcoming_launch['date_local'],
+                                        rocket['name'],
+                                        launchpad['name'],
+                                        launchpad['locality']))
