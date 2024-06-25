ee815c860a8a7b2628cd7a9c635c22b349acfb71
chaouch <chaouch.thameurtc61@gmail.com>
2024-06-25 09:49:11 +0100

    gan

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -30,7 +30,8 @@ class Simple_GAN(keras.Model):
         self.beta_2 = .9
 
         # define the generator loss and optimizer:
-        self.generator.loss = lambda x: tf.keras.losses.MeanSquaredError()(x, tf.ones(x.shape))
+        self.generator.loss = lambda x: tf.keras.losses.MeanSquaredError()\
+            (x, tf.ones(x.shape))
         self.generator.optimizer = keras.optimizers.Adam(
             learning_rate=self.learning_rate,
             beta_1=self.beta_1,
@@ -41,7 +42,8 @@ class Simple_GAN(keras.Model):
         # define the discriminator loss and optimizer:
         self.discriminator.loss = \
             lambda x, y: tf.keras.losses.MeanSquaredError()(
-                x, tf.ones(x.shape)) + tf.keras.losses.MeanSquaredError()(y, -1*tf.ones(y.shape))
+                x, tf.ones(x.shape)) + tf.keras.losses.MeanSquaredError()\
+                    (y, -1*tf.ones(y.shape))
         self.discriminator.optimizer = keras.optimizers.Adam(
             learning_rate=self.learning_rate,
             beta_1=self.beta_1,
